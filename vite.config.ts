4d0f932cbd647790c781d0253dc05859b4a60601
Alex Burner <aburner@apple.com>
2022-07-04 11:11:46 -0700

    Add .vscode

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig, PluginOption } from 'vite'
+import { defineConfig } from 'vite'
 import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
